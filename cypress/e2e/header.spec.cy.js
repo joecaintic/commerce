991a0d618c7195ebf35c16027e0667b1e9f8cfd0
Joseph Caintic <joseph.caintic@hilton.com>
2023-12-01 11:27:18 -0500

    putting back first method in search bar test

--- cypress/e2e/header.spec.cy.js
+++ cypress/e2e/header.spec.cy.js
@@ -12,9 +12,11 @@ describe('Header', () => {
     cy.getBySel('search-input').eq(0).type('star');
     cy.getBySel('search-input').eq(0).realPress('Enter');
 
-    cy.get('[data-test="product-tag"]').within(() => {
-      cy.get('[data-test="product-name"]').should('contain', 'Star Wars');
-      cy.get('[data-test="product-price"]').should('contain', '$25.00USD');
-    });
+    cy.get('[data-test="product-tag"]')
+      .first()
+      .within(() => {
+        cy.get('[data-test="product-name"]').should('contain', 'Star Wars');
+        cy.get('[data-test="product-price"]').should('contain', '$25.00USD');
+      });
   });
 });
